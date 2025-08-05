727628879384935264e7ed8170914c33b1239b10
Olowokere_Destiny <destinyolowokere@gmail.com>
2025-08-05 18:34:06 +0100

    changed home image

--- client/src/pages/main/DefaultHome.tsx
+++ client/src/pages/main/DefaultHome.tsx
@@ -1,15 +1,10 @@
+import cover from "../../assets/ws-bg.png"
 const DefaultHome = () => {
   return (
     <div className="h-full">
-      <div className="h-full w-full flex flex-col items-center justify-center text-center cover">
-        <div className="text-center space-y-4">
-          <h1 className="theme-text-gradient text-4xl lg:text-5xl font-bold">
-            Welcome to Teem
-          </h1>
-          <p className="text-slate-600 text-md font-medium">
-            Collaborate with your team in real-time
-          </p>
-        </div>
+      <div className="h-full w-full flex flex-col items-center justify-center text-center">
+        <img src={cover} className="size-80" />
+        <p className="text-sm font-medium text-slate-600">Collaborate with your team in real-time</p>
       </div>
     </div>
   );
